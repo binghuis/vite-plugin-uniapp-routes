46d56450dac37261d8b6e7274873f7462e4fb2d4
宋秉徽 <451000936@qq.com>
2024-03-27 19:41:23 +0800

    feat: 代码压缩

--- example/uniapp-vue3/src/routes.ts
+++ example/uniapp-vue3/src/routes.ts
@@ -1,17 +1,13 @@
 /* eslint-disable */
-
 export enum RoutesEnum {
-  /** 首页 */
-  "IndexIndex" = "/pages/index/index",
-  /** 登录 */
-  "LoginIndex" = "/pages/login/index",
-  /** 我的 */
-  "MineIndex" = "/pages/mine/index",
+	/** 首页 */
+	"IndexIndex" = "/pages/index/index",
+	/** 登录 */
+	"LoginIndex" = "/pages/login/index",
+	/** 我的 */
+	"MineIndex" = "/pages/mine/index"
 }
 
-export type RouteKeyType = "IndexIndex" | "LoginIndex" | "MineIndex";
+export type RouteKeyType = "IndexIndex" | "LoginIndex" | "MineIndex"
 
-export type RoutePathType =
-  | "/pages/index/index"
-  | "/pages/login/index"
-  | "/pages/mine/index";
+export type RoutePathType = "/pages/index/index" | "/pages/login/index" | "/pages/mine/index"
